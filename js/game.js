80dfa83530055a74c698bf642fd23af204e89443
Marianna <ma_rianna@outlook.com>
2023-07-02 18:50:54 -0300

    Updating game page.

--- js/game.js
+++ js/game.js
@@ -1,16 +1,18 @@
-const grid = document.querySelector(".grid");
+const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
-  "beth",
-  "jerry",
-  "jessica",
-  "morty",
-  "pessoa-passaro",
-  "pickle-rick",
-  "rick",
-  "summer",
-  "meeseeks",
-  "scroopy",
+  'beth',
+  'jerry',
+  'jessica',
+  'morty',
+  'pessoa-passaro',
+  'pickle-rick',
+  'rick',
+  'summer',
+  'meeseeks',
+  'scroopy',
 ];
 
 const createElement = (tag, className) => {
@@ -19,50 +21,51 @@ const createElement = (tag, className) => {
   return element;
 };
 
-let firstCard = "";
-let secondCard = "";
+let firstCard = '';
+let secondCard = '';
 
 const checkEndGame = () => {
-  const disabledCards = document.querySelectorAll(".disabled-card");
+  const disabledCards = document.querySelectorAll('.disabled-card');
 
   if (disabledCards.length === 20) {
-    alert("Parabéns, você ganhou!");
+    clearInterval(this.loop);
+    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
   }
 };
 
 const checkCards = () => {
-  const firstCharacter = firstCard.getAttribute("data-character");
-  const secondCharacter = secondCard.getAttribute("data-character");
+  const firstCharacter = firstCard.getAttribute('data-character');
+  const secondCharacter = secondCard.getAttribute('data-character');
 
   if (firstCharacter === secondCharacter) {
-    firstCard.firstChild.classList.add("disabled-card");
-    secondCard.firstChild.classList.add("disabled-card");
+    firstCard.firstChild.classList.add('disabled-card');
+    secondCard.firstChild.classList.add('disabled-card');
 
-    firstCard = "";
-    secondCard = "";
+    firstCard = '';
+    secondCard = '';
 
     checkEndGame();
   } else {
     setTimeout(() => {
-      firstCard.classList.remove("reveal-card");
-      secondCard.classList.remove("reveal-card");
+      firstCard.classList.remove('reveal-card');
+      secondCard.classList.remove('reveal-card');
 
-      firstCard = "";
-      secondCard = "";
+      firstCard = '';
+      secondCard = '';
     }, 500);
   }
 };
 
 const revealCard = ({ target }) => {
-  if (target.parentNode.className.includes("reveal-card")) {
+  if (target.parentNode.className.includes('reveal-card')) {
     return;
   }
 
-  if (firstCard === "") {
-    target.parentNode.classList.add("reveal-card");
+  if (firstCard === '') {
+    target.parentNode.classList.add('reveal-card');
     firstCard = target.parentNode;
-  } else if (secondCard === "") {
-    target.parentNode.classList.add("reveal-card");
+  } else if (secondCard === '') {
+    target.parentNode.classList.add('reveal-card');
     secondCard = target.parentNode;
 
     checkCards();
@@ -70,17 +73,17 @@ const revealCard = ({ target }) => {
 };
 
 const createCard = (character) => {
-  const card = createElement("div", "card");
-  const front = createElement("div", "face front");
-  const back = createElement("div", "face back");
+  const card = createElement('div', 'card');
+  const front = createElement('div', 'face front');
+  const back = createElement('div', 'face back');
 
   front.style.backgroundImage = `url('../assets/${character}.png')`;
 
   card.appendChild(front);
   card.appendChild(back);
 
-  card.addEventListener("click", revealCard);
-  card.setAttribute("data-character", character);
+  card.addEventListener('click', revealCard);
+  card.setAttribute('data-character', character);
 
   return card;
 };
@@ -96,4 +99,19 @@ const loadGame = () => {
   });
 };
 
-loadGame();
+const startTimer = () => {
+
+  this.loop = setInterval(() => {
+
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+
+  }, 1000);
+}
+
+window.onload = () => {
+
+  spanPlayer.innerHTML = localStorage.getItem('player');
+  startTimer();
+  loadGame();
+}
